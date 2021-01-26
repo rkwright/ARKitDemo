ade3656c778174eab08749151e2fc069789e1562
rkwright <rkwright@geofx.com>
2021-01-26 15:50:45 -0800

    More or less working now with a spinning cube over the camera's scene.  Seems kind of pointless to me but I'm not a video guy.  Anyway, some cleanup still needed but otherwise "complete".

--- ARKitDemo/ViewController.swift
+++ ARKitDemo/ViewController.swift
@@ -14,9 +14,51 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //addLights()
+        sceneView.autoenablesDefaultLighting = true
+
+        addBox()
     }
 
+    func addBox() {
+        let box = SCNBox(width: 0.025, height: 0.025, length: 0.025, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(0, 0, -0.0)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        box.materials = [material]
+        
+        rotateObject(rotation: 0.5, planet: boxNode, duration: 0.25)
+            
+        let scene = SCNScene()
+        scene.rootNode.addChildNode(boxNode)
+        sceneView.scene = scene
+    }
+    
+    func rotateObject(rotation: Float, planet: SCNNode, duration: Float){
+        let rotation = SCNAction.rotateBy(x:CGFloat(rotation),y:CGFloat(rotation),z:CGFloat(rotation), duration: TimeInterval(duration))
+        planet.runAction(SCNAction.repeatForever(rotation))
+    }
+
+    func addLights() {
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = SCNLight.LightType.ambient
+        ambientLightNode.light!.color = UIColor(white: 0.67, alpha: 1.0)
+        sceneView.scene.rootNode.addChildNode(ambientLightNode)
+        
+        let omniLightNode = SCNNode()
+        omniLightNode.light = SCNLight()
+        omniLightNode.light!.type = SCNLight.LightType.omni
+        omniLightNode.light!.color = UIColor(white: 0.75, alpha: 1.0)
+        omniLightNode.position = SCNVector3Make(0, 50, 50)
+        sceneView.scene.rootNode.addChildNode(omniLightNode)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let configuration = ARWorldTrackingConfiguration()
